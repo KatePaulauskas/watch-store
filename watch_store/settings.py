946973348d894dd0aa15130f2373b7c6047f0649
Ekaterina Mikheeva <ekaterina.paulauskas@gmail.com>
2024-08-13 17:17:50 +0000

    style: set header & above-fold for media queries, add favicon

--- watch_store/settings.py
+++ watch_store/settings.py
@@ -124,8 +124,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+IMAGES_URL = '/images/'
+IMAGES_ROOT = os.path.join(BASE_DIR, 'images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

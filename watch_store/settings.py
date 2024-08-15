70e092a3298094c94c220d9b03c8e20585eedb95
Ekaterina Mikheeva <ekaterina.paulauskas@gmail.com>
2024-08-15 14:26:13 +0000

    docs: update README with media files

--- watch_store/settings.py
+++ watch_store/settings.py
@@ -124,8 +124,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-IMAGES_URL = '/images/'
-IMAGES_ROOT = os.path.join(BASE_DIR, 'images')
+MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

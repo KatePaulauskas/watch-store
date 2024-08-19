502bdf176c7ed7ea2e0b8bb75a842898d235c538
Ekaterina Mikheeva <ekaterina.paulauskas@gmail.com>
2024-08-19 22:00:50 +0000

    feat: product page template: customise, set responsive style

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 def shop(request):
@@ -11,3 +11,15 @@ def shop(request):
     }
 
     return render(request, 'shop/shop.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'shop/product_page.html', context)

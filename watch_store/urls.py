ebdb4f969c5e1434b80409cf381c305b7ef1ffa8
Ekaterina Mikheeva <ekaterina.paulauskas@gmail.com>
2024-08-27 14:46:07 +0000

    feat: set checkout view and responsive template

--- watch_store/urls.py
+++ watch_store/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls')),
+    path('checkout/', include('checkout.urls')),
     path('shop/', include('shop.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

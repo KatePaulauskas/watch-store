f39d1f60f8b9e5910ce123b61704256b515ab932
Ekaterina Mikheeva <ekaterina.paulauskas@gmail.com>
2024-08-29 17:03:53 +0000

    feat: implement webhook handler for Stripe events

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('thank_you/<order_number>', views.checkout_success, name='thank_you'),
+    path('wh/', webhook, name='webhook'),
 ]

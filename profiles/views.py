ec8146ba42ac8c8741e4016423558c1e0b14f329
Ekaterina Mikheeva <ekaterina.paulauskas@gmail.com>
2024-09-06 16:51:12 +0000

    feat: set product upload functionality

--- profiles/views.py
+++ profiles/views.py
@@ -14,9 +14,11 @@ def profile(request):
         form = UserProfileForm(request.POST, instance=profile)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Profile updated successfully')
-
-    form = UserProfileForm(instance=profile)
+            messages.success(request, 'Profile was updated successfully!')
+        else:
+            message.error(request, 'Profile update has failed. Please, ensure the form is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'

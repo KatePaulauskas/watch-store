a11ccbe7262b3f546db1f0db4b2045164f0500fb
Ekaterina Mikheeva <ekaterina.paulauskas@gmail.com>
2024-09-07 10:55:52 +0000

    feat: set product delete functionality

--- shop/views.py
+++ shop/views.py
@@ -82,9 +82,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Product is added successfully!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'There was a problem adding your product, ensure the form is valid.')
     else:
@@ -127,6 +127,14 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted successfully!')
+
+    return redirect(reverse('shop'))
 
 def cancel_action(request):
     messages.add_message(request, messages.INFO,

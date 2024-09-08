60bfb00eea99f76c36b314446020f41e846c31c7
Ekaterina Mikheeva <ekaterina.paulauskas@gmail.com>
2024-09-08 12:45:39 +0000

    feat: set admin functionality for store policies editing

--- policies/urls.py
+++ policies/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.policies_display, name='policies'),
+    path('edit/<int:pk>/', views.edit_policy, name='edit_policy'),
+    path('cancel_editing/', views.cancel_editing, name='cancel_editing'),
 ]
--- policies/views.py
+++ policies/views.py
@@ -1,6 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import Policy
+from .forms import PolicyForm
 
 def policies_display(request):
     policies = Policy.objects.all()
     return render(request, 'policies/policies.html', {'policies': policies})
+
+@login_required
+def edit_policy(request, pk):
+    policy = get_object_or_404(Policy, pk=pk)
+    if request.method == 'POST':
+        form = PolicyForm(request.POST, instance=policy)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'{policy.title} updated successfully!')
+            return redirect('policies')
+    else:
+        form = PolicyForm(instance=policy)
+    
+    return render(request, 'policies/edit_policy.html', {'form': form, 'policy': policy})
+
+def cancel_editing(request):
+    """ Cancel delete action on she shop page """
+    messages.add_message(request, messages.INFO,
+                         "Action cancelled. No changes were made.")
+
+    return redirect('policies')
